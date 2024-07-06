8f977800c5cf50cc15fc5029f92a908caac89ba8
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-06 19:23:53 -0300

    feat: add AuthController

---
+++ src/main/java/com/wesleybertipaglia/blog/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.wesleybertipaglia.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesleybertipaglia.blog.dtos.SignInRequest;
+import com.wesleybertipaglia.blog.dtos.SignInResponse;
+import com.wesleybertipaglia.blog.dtos.SignUpRequest;
+import com.wesleybertipaglia.blog.dtos.SignUpResponse;
+import com.wesleybertipaglia.blog.services.AuthService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<SignInResponse> signin(@RequestBody SignInRequest signInRequest) {
+        return ResponseEntity.of(authService.signin(signInRequest));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpResponse> signup(@RequestBody SignUpRequest signUpRequest) {
+        return ResponseEntity.of(authService.signup(signUpRequest));
+    }
+}
