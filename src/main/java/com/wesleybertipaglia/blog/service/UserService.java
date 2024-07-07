0df1063910832774bc71b64492113d40fd4d5db5
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-06 21:19:33 -0300

    feat: add UserService and listUsers method

---
+++ src/main/java/com/wesleybertipaglia/blog/service/UserService.java
@@ -0,0 +1,24 @@
+package com.wesleybertipaglia.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.wesleybertipaglia.blog.dtos.UserResponse;
+import com.wesleybertipaglia.blog.mapper.UserMapper;
+import com.wesleybertipaglia.blog.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public Page<UserResponse> listUsers(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return userRepository.findAll(pageable).map(UserMapper::convertToDTO);
+    }
+}
