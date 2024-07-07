7e26bc9ed834c4555eb13eb1e40dba987fab3c7f
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-07 12:17:32 -0300

    feat: add PostMapper

---
+++ src/main/java/com/wesleybertipaglia/blog/mapper/PostMapper.java
@@ -0,0 +1,39 @@
+package com.wesleybertipaglia.blog.mapper;
+
+import com.wesleybertipaglia.blog.dtos.post.PostCreateDTO;
+import com.wesleybertipaglia.blog.dtos.post.PostResponseDTO;
+import com.wesleybertipaglia.blog.model.Post;
+import com.wesleybertipaglia.blog.model.User;
+
+public class PostMapper {
+    public static PostResponseDTO convertToDTO(Post post) {
+        if (post == null) {
+            return null;
+        } else if (post.getId() == null) {
+            throw new IllegalArgumentException("Post ID is required");
+        } else if (post.getCreator() == null) {
+            throw new IllegalArgumentException("Post creator is required");
+        } else if (post.getTitle() == null) {
+            throw new IllegalArgumentException("Post title is required");
+        } else if (post.getContent() == null) {
+            throw new IllegalArgumentException("Post content is required");
+        }
+
+        return new PostResponseDTO(post.getId(), post.getCreator().getUsername(), post.getTitle(), post.getContent(),
+                post.getLikes());
+    }
+
+    public static Post convertToEntity(PostCreateDTO postCreateDTO, User user) {
+        if (postCreateDTO == null) {
+            throw new IllegalArgumentException("Post is required");
+        } else if (user == null) {
+            throw new IllegalArgumentException("User is required");
+        } else if (postCreateDTO.title() == null) {
+            throw new IllegalArgumentException("Post title is required");
+        } else if (postCreateDTO.content() == null) {
+            throw new IllegalArgumentException("Post content is required");
+        }
+
+        return new Post(user, postCreateDTO.title(), postCreateDTO.content());
+    }
+}
