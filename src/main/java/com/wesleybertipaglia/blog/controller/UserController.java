37e097b25eb614696f3900499bdf963763f4eaf4
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-06 21:19:50 -0300

    feat: add UserController and listUsers method

---
+++ src/main/java/com/wesleybertipaglia/blog/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.wesleybertipaglia.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesleybertipaglia.blog.dtos.UserResponse;
+import com.wesleybertipaglia.blog.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<Page<UserResponse>> listUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(userService.listUsers(page, size));
+    }
+}
