da693b31caa6b8f44dfc71be4e77a48a42c947ec
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-07 12:18:22 -0300

    feat: add createPost in PostController

--- src/main/java/com/wesleybertipaglia/blog/controller/PostController.java
+++ src/main/java/com/wesleybertipaglia/blog/controller/PostController.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wesleybertipaglia.blog.model.Post;
+import com.wesleybertipaglia.blog.dtos.post.PostCreateDTO;
+import com.wesleybertipaglia.blog.dtos.post.PostResponseDTO;
 import com.wesleybertipaglia.blog.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/posts")
@@ -18,9 +22,14 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @PostMapping
+    public ResponseEntity<PostResponseDTO> createPost(@RequestBody PostCreateDTO post, JwtAuthenticationToken token) {
+        return ResponseEntity.of(postService.createPost(post, token.getName()));
+    }
+
     @GetMapping
     @PreAuthorize("permitAll()")
-    public ResponseEntity<Page<Post>> listPosts(
+    public ResponseEntity<Page<PostResponseDTO>> listPosts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         return ResponseEntity.ok(postService.listPosts(page, size));
