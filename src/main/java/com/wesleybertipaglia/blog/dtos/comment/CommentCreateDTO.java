c023dd8ea976c3a9cf866997cfeab3ab38566fcf
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-11 19:04:07 -0300

    style: add content parameter to comment records

--- src/main/java/com/wesleybertipaglia/blog/dtos/comment/CommentCreateDTO.java
+++ src/main/java/com/wesleybertipaglia/blog/dtos/comment/CommentCreateDTO.java
@@ -2,5 +2,5 @@ package com.wesleybertipaglia.blog.dtos.comment;
 
 import java.util.UUID;
 
-public record CommentCreateDTO(UUID id, UUID userId, UUID postId) {
+public record CommentCreateDTO(UUID id, UUID userId, UUID postId, String content) {
 }
--- src/main/java/com/wesleybertipaglia/blog/dtos/comment/CommentResponseDTO.java
+++ src/main/java/com/wesleybertipaglia/blog/dtos/comment/CommentResponseDTO.java
@@ -2,5 +2,5 @@ package com.wesleybertipaglia.blog.dtos.comment;
 
 import java.util.UUID;
 
-public record CommentResponseDTO(UUID id, UUID userId, UUID postId) {
+public record CommentResponseDTO(UUID id, UUID userId, UUID postId, String content) {
 }
