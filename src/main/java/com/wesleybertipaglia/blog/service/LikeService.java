3e40cbd401011a01375f0a34d14d98641c583e96
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-11 12:27:30 -0300

    feat: add list methods to LikeService

--- src/main/java/com/wesleybertipaglia/blog/service/LikeService.java
+++ src/main/java/com/wesleybertipaglia/blog/service/LikeService.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.wesleybertipaglia.blog.dtos.like.LikeResponseDTO;
@@ -43,5 +46,17 @@ public class LikeService {
         }
 
         return Optional.of(LikeMapper.convertToDTO(likeRepository.save(like)));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<LikeResponseDTO> listLikesByPost(int page, int size, UUID postId) {
+        Pageable pageable = PageRequest.of(page, size);
+        return likeRepository.findByPostId(postId, pageable).map(LikeMapper::convertToDTO);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<LikeResponseDTO> listLikesByUser(int page, int size, UUID userId) {
+        Pageable pageable = PageRequest.of(page, size);
+        return likeRepository.findByUserId(userId, pageable).map(LikeMapper::convertToDTO);
     }
 }
