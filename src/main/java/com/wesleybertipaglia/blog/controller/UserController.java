e4c405ec901c2cee0cd673eb0f47844a0d237ab3
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-11 18:36:03 -0300

    feat: add getUserById in UserController and UserService

--- src/main/java/com/wesleybertipaglia/blog/controller/UserController.java
+++ src/main/java/com/wesleybertipaglia/blog/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.wesleybertipaglia.blog.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wesleybertipaglia.blog.dtos.user.UserResponseDTO;
 import com.wesleybertipaglia.blog.service.UserService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -24,5 +28,11 @@ public class UserController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         return ResponseEntity.ok(userService.listUsers(page, size));
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 }
--- src/main/java/com/wesleybertipaglia/blog/service/UserService.java
+++ src/main/java/com/wesleybertipaglia/blog/service/UserService.java
@@ -1,5 +1,7 @@
 package com.wesleybertipaglia.blog.service;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.domain.Page;
@@ -10,15 +12,26 @@ import org.springframework.stereotype.Service;
 import com.wesleybertipaglia.blog.dtos.user.UserResponseDTO;
 import com.wesleybertipaglia.blog.mapper.UserMapper;
 import com.wesleybertipaglia.blog.repository.UserRepository;
+import com.wesleybertipaglia.blog.repository.LikeRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private LikeRepository likeRepository;
+
     @Transactional(readOnly = true)
     public Page<UserResponseDTO> listUsers(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return userRepository.findAll(pageable).map(UserMapper::convertToDTO);
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponseDTO getUserById(UUID id) {
+        return UserMapper.convertToDTO(userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found")));
     }
 }
