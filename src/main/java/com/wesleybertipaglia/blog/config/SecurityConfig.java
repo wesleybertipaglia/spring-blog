f97cb39f6158af321150ca09d3b2200c9426bc80
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-11 20:40:54 -0300

    feat: add requestMatchers for comments in SecurityConfig

--- src/main/java/com/wesleybertipaglia/blog/config/SecurityConfig.java
+++ src/main/java/com/wesleybertipaglia/blog/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/signup", "/auth/signin").permitAll()
                         .requestMatchers("/users", "/users/{id}").permitAll()
                         .requestMatchers("/posts", "/posts/{id}", "/posts/{id}/likes").permitAll()
+                        .requestMatchers("/comments", "/comments/{id}", "/comments/post/{id}").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
