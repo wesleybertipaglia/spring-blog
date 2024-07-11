cb71f8d1e2df83270279806b28b7dc8caa2878cf
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-11 13:07:54 -0300

    feat: add handleIllegalArgumentException to GlobalExceptionHandler

--- src/main/java/com/wesleybertipaglia/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/wesleybertipaglia/blog/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.wesleybertipaglia.blog.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
