b65465b73f697fb224198ed486c4fc9e4ffe2781
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-11 21:55:05 -0300

    style: rm params from LikeCreateDTO

--- src/main/java/com/wesleybertipaglia/blog/dtos/like/LikeCreateDTO.java
+++ src/main/java/com/wesleybertipaglia/blog/dtos/like/LikeCreateDTO.java
@@ -2,5 +2,5 @@ package com.wesleybertipaglia.blog.dtos.like;
 
 import java.util.UUID;
 
-public record LikeCreateDTO(UUID id, UUID userId, UUID postId) {
+public record LikeCreateDTO(UUID postId) {
 }
